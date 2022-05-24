dbfe432da3882c090c563708cc18d19937fdb8e9
Justin <justinmathiue0@gmail.com>
2022-05-24 10:23:36 -0700

    make it better

--- server.js
+++ server.js
@@ -10,39 +10,74 @@ const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3002;
 
 app.get('/', (request, response) => {
-    response.send('testing');
+
+	response.send('testing');
 });
 
 
 app.get('/weather', async (request, response, next) => {
-    try {
+	try {
+		const locationLat = request.query.lat;
 
-        const locationLat = request.query.locationLat;
-        const locationLon = request.query.locationLon;
-        console.log(`this is the first console.log${locationLat}`);
-        const url = process.env.WEATHER_URL;
-        const weatherResults = await axios.get(`${url}?key=${process.env.WEATHER_API_KEY}&lat=${locationLat}lon=${locationLon}`);
-        // const cityWeather = new Forecast(weatherResults);
-        console.log(weatherResults);
-        // response.status(200).send(cityWeather);
-    } catch (error) {
-        error.customMessage = 'something happened in the api';
-        next(error);
-    }
+		const locationLon = request.query.lon;
+
+		const url = process.env.WEATHER_URL;
+		const weatherResults = await axios.get(`${url}?lat=${locationLat}&lon=${locationLon}&key=${process.env.WEATHER_API_KEY}&days=3`);
+		const packagedResults = (weatherResults.data.data.map(forecast => new Forecast(forecast)));
+		response.status(200).send(packagedResults);
+	} catch (error) {
+		error.customMessage = 'something happened in the weather api';
+		next(error);
+	}
 });
 
 
-// class Forecast {
-//   constructor(weatherResults) {
-//     this.city = Forecast.weatherResults.find(obj => obj.city_name.toLowerCase() === city.toLowerCase());
-//     this.cityArr = this.city.data.map(obj => ({ 'date': obj.datetime, 'description': obj.weather.description }));
-//   }
-// }
+app.get('/movies', async (request, response, next) => {
+	try {
+		const search = request.query.search;
+		console.log('is this working???', search);
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
+		const movieResults = await axios.get(url);
+		const packagedMovieResults = (movieResults.data.results.map(movie => new Movie(movie)));
+		console.log(movieResults.data.results[0]);
+		response.status(200).send(packagedMovieResults);
+	} catch (error) {
+		error.customMessage = 'something happened in the movie api';
+		next(error);
+	}
+});
+
+class Forecast {
+	constructor(forecast) {
+		this.low = forecast.low_temp,
+			this.description = forecast.weather.description,
+			this.high = forecast.max_temp,
+			this.date = forecast.valid_date;
+
+	}
+}
+
+class Movie {
+	constructor(movie) {
+		this.title = movie.title,
+			this.overview = movie.overview,
+			this.avgVotes = movie.vote_average,
+			this.image = movie.poster_path,
+			this.pop = movie.popularity,
+			this.release = movie.release_date;
+
+	}
+}
+
+
+
+
+
 app.use((error, request, response,) => {
-    response.status(500).send(`something went wrong ${error.customMessage}`);
+	response.status(500).send(`something went wrong ${error.customMessage}`);
 });
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
